7448f517681c3ca87f3fc880a7404ae1a251434d
Ananthan Manikandan <101982906+Anandoptimus@users.noreply.github.com>
2025-04-28 17:46:49 +0530

    Create extraction_chains.py

---
+++ extraction_chains.py
@@ -0,0 +1,27 @@
+from typing import Optional
+
+class Person(BaseModel):
+    """Infomation about a person"""
+    name: str = Field(description = "person's name")
+    age: Optional[int] = Field(description = "person's age")
+
+class Infomation(BaseModel):
+    """Infomation to extract"""
+    person: List[Person] = Field(description = "List of infomation about person")
+
+infomation_function = [convert_pydantic_to_openai_function(Infomation)]
+
+information_model = model.bind(functions = infomation_function, function_call = {"name": "Infomation"})
+
+prompt = ChatPromptTemplate.from_messages([
+    ("system", "extract the relevant details, if not explicitly provide do not guess. Extract partial info"),
+    ("user", "{input}")
+])
+
+chain1 = prompt | information_model | JsonOutputFunctionsParser()
+
+chain1.invoke({"input": "i am anand, my age is 23"})
+
+chain1.invoke({"input": "my sister name is abi, she is 21"})
+
+chain1.batch([{"input": "i am anand, my age is 23"}, {"input": "my sister name is abi, she is 21"}])
